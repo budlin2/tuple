fe09997effbcc60c013164e34c13a9cb447f9301
budlinville <budlinville@gmail.com>
2022-12-26 22:23:15 -0600

    Scrollbar now fades away

--- src/components/ScrollPane/index.tsx
+++ src/components/ScrollPane/index.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, ReactElement } from 'react';
+import { CSSProperties, ReactElement, useState, useEffect } from 'react';
 
 import _classes from './ScrollPane.module.css';
 
@@ -15,10 +15,23 @@ const ScrollPane = ({
     className,
     children,
 }: Props) => {
-    const _className = `${_classes.scrollpane} ${className}`;
+    const scrollPaneClasses = `${_classes.scrollpane} ${className}`;
+    const [_className, setClassName] = useState(scrollPaneClasses);
+    const [isScrolling, setIsScrolling] = useState(false);
+
+    const onScrollHandler = () => setIsScrolling(true);
+
+    useEffect(() => {
+        if (isScrolling) {
+           setClassName(scrollPaneClasses);
+           setTimeout(() => setIsScrolling(false), 1000);
+        } else {
+            setClassName(`${scrollPaneClasses} ${_classes.scrollpaneHidden}`);
+        }
+    }, [isScrolling]);
 
     return (
-        <div className={_className} style={style}>
+        <div className={_className} style={style} onScroll={onScrollHandler}>
             { children }
         </div>
     );
