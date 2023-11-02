1ce4df9af0e4ff4add3d7cbaf83ce7806a6cca8b
budlinville <budlinville@gmail.com>
2023-11-01 20:33:16 -0500

    Updated Storybook and Vite

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,52 +1,32 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
-}
-
-// import React from 'react';
-// import { configure, addDecorator } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import styled from 'styled-components';
-
-// const Wrapper = styled.div`
-//   display: flex;
-//   flex: 1;
-//   height: 100vh;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// const Decorator = (storyFn) => <Wrapper>{storyFn()}</Wrapper>;
-// addDecorator(Decorator);
-
-// global.___loader = {
-//   enqueue: () => {},
-//   hovering: () => {},
-// };
-
-// global.__PATH_PREFIX__ = '';
-// window.___navigate = (pathname) => {
-//   action('NavigateTo:')(pathname);
-// };
-
-// const req = require.context('../src/components/', true, /\.stories\.js$/);
-
-// function loadStories() {
-//   req.keys().forEach((filename) => req(filename));
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.stories.mdx",
+//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions"
+//   ],
+//   "framework": "@storybook/react",
+//   "core": {
+//     "builder": "@storybook/builder-vite"
+//   },
+//   "features": {
+//     "storyStoreV7": true
+//   }
 // }
 
-// configure(loadStories, module);
+export default {
+  stories: [
+    '../src/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+};
