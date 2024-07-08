faadcc9478320d982f28e41bb8c9a22bff6fecc5
budlinville <budlinville@gmail.com>
2024-07-07 20:05:31 -0500

    0.1.7
    Exporting splitpane

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export { default as Tuple } from '../components/Tuple';
 export { default as Page } from '../components/Page';
 export { default as Bubble } from '../components/Bubble';
+export { default as SplitPane } from '../components/SplitPane';
 
 import lannister from '../components/Tuple/templates/lannister.module.css';
 import baratheon from '../components/Tuple/templates/baratheon.module.css';
